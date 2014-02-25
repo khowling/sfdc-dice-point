fa2d4c51edbc5cd843f7f78fa9bdad3c81da825c
khowling@gmail.com <khowling@gmail.com>
2014-02-25 12:08:17 +0000

    savepoint

--- src/PointIntegration.java
+++ src/PointIntegration.java
@@ -1,6 +1,18 @@
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+
 import com.sforce.soap.enterprise.*;
+import com.mdsuk.ws.dise3g.address.dto.business.AddressType;
+import com.mdsuk.ws.dise3g.address.dto.business.BusinessAddressType;
 import com.mdsuk.ws.dise3g.order.service.*;
+import com.mdsuk.ws.dise3g.order.dto.business.ActivationType;
 import com.mdsuk.ws.dise3g.order.dto.business.OrderHeaderType;
+import com.mdsuk.ws.dise3g.order.dto.business.PaymentType;
+import com.mdsuk.ws.dise3g.order.dto.business.ShippingAddressInformationType;
 import com.mdsuk.ws.dise3g.order.dto.service.*;
 import com.mdsuk.ws.dise3g.subscription.dto.business.SubscriptionConnectionType;
 import com.sforce.soap.enterprise.sobject.*;
@@ -10,8 +22,9 @@ import com.sforce.ws.*;
 public class PointIntegration {
 
 	
-	public static void main(String[] args) throws ConnectionException {
-		System.out.println ("Staring...");
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) throws ConnectionException, DatatypeConfigurationException {
+		System.out.println ("Starting...");
 		ConnectorConfig config = new ConnectorConfig();
 		config.setUsername(System.getenv("SFDC_USERNAME"));
 		config.setPassword(System.getenv("SFDC_PASSWORD"));
@@ -21,26 +34,67 @@ public class PointIntegration {
 		OrderService dice = new OrderService();
 		OrderPortType diceSoap = dice.getOrderPort();
 
-		QueryResult qr = connection.query("Select subject from Case where MDS_Status__c = 'Pending'");
+		QueryResult qr = connection.query("Select subject, Account.AccountNumber from Case where MDS_Status__c = 'Pending'");
 
 		SObject[] recs = qr.getRecords();
 		for (SObject o : recs) {
 			Case c= (Case)o;
+			
 			System.out.println ("Got Case " + c.getSubject());
 			
 			OrderType ot = new OrderType();
-			ot.setAccountIdentifier(123123);
 			
+			// DISE Account number - NR Account represents the Account Cost centre
+			// this is the Accont.AccountNumber
+			ot.setAccountIdentifier(4968558); 
+
 			ot.setExternalReference(c.getId());
 			
 			OrderHeaderType oht = new OrderHeaderType();
+
+			// Phone, Name, payroll number + cost centre
 			oht.setCustomerReference("Customer");
-			oht.setDeliveryInstructions("Del Instructions");
+			
+			oht.setDeliveryInstructions("Not used");
+
+
+
+			XMLGregorianCalendar xmlDate = DatatypeFactory.newInstance().newXMLGregorianCalendar(); 
+			xmlDate.setYear(new Date().getYear());
+			xmlDate.setMonth(new Date().getMonth());
+			xmlDate.setDay(new Date().getDay());
+			
+			oht.setDespatchByDate(xmlDate);
+			oht.setGenerate(true);
+		//	PaymentType pt = new PaymentType();
+			oht.setPaymentType(PaymentType.ON_ACCOUNT);
+			oht.setPromisedForDate(xmlDate);
+			
+			ShippingAddressInformationType s = new ShippingAddressInformationType();
+			AddressType at = new AddressType();
+			BusinessAddressType ba = new BusinessAddressType();
+			ba.setAddress1("Addr1");
+			ba.setAddress2("Addr2");
+			ba.setPostcode("SL6 1HH");
+			at.setBusinessAddress(ba);
+			s.setAddress(at);
+			
+			oht.setShippingAddressInformation(s);
+			// new field on the Case SubscriptonAccount.AccountNumber
+			oht.setSubscriptionAccountCode(arg0);
+			oht.setWarehouseCode("HOWLEY");
+			
 			
 			ot.setOrderHeader(oht);
 			
 			ProductType pt = new ProductType();
+			
+			pt.setActivationType(ActivationType.IMMEDIATE);
+			//pt.setCategory(arg0);
+			//pt.setComment(arg0);
+			pt.setProductPriceOverride(arg0);
 			pt.setProductCode("CODE01");
+			pt.setSubscription(arg0);
 			pt.setQuantity(1);
 			pt.setCategory("Category");
 			
@@ -52,17 +106,29 @@ public class PointIntegration {
 			
 			ot.setProducts(pst);
 			System.out.println ("Created dice OrderType, sending... ");
+			
+			Case newc = new Case();
+			newc.setId(c.getId());
+			
 			try { 
 				OrderResponseType ores = diceSoap.createOrder(ot);
 				String onum = ores.getOrderNumber();
-				
-				Case newc = new Case();
-				newc.setId(c.getId());
+
 				newc.setBilling_Order_Number__c(onum);
 				connection.update(new Case[]{newc});
 				
-			} catch (Exception e) {
-				System.out.println ("exception " + e.getMessage());
+				System.out.println ("Created");
+				
+			} catch (Exception ex1) {
+				
+				System.out.println ("exception, updating salesforce with " + ex1.getMessage());
+				
+				try {
+					newc.setMDS_Status__c(ex1.getMessage());
+					connection.update(new Case[]{newc});
+				} catch (Exception ex2) { // unable to update
+					System.out.println ("exception trying to update status in salesforce" + ex2.getMessage());
+				}
 			}
 			
 		}
